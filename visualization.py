eb152ca5bba8352fb5b2da5c9d740d11d778e79a
meghakn23 <meghakn03@gmail.com>
2024-10-25 15:51:34 +0100

    graph update

--- visualization.py
+++ visualization.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
 
-# Real-time plotting for anomalies
 def plot_stream(data, anomalies):
     plt.ion()
     fig, ax = plt.subplots()
 
     anomaly_indices = [i for i, point in enumerate(data) if point in anomalies]
+    
     for i in range(len(data)):
         ax.clear()
-        ax.plot(data[:i+1], label="Data Stream")
+        ax.plot(data[:i + 1], label="Data Stream", color='blue')
 
         # Highlight anomalies
         if i in anomaly_indices:
-            ax.plot(i, data[i], "ro", label="Anomaly")
+            ax.plot(i, data[i], "ro", label="Anomaly", markersize=8)  # Increased marker size
 
         ax.legend()
         plt.pause(0.05)
